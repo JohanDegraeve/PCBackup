9b21d27eda9a7c9afab26df37f95db63d1f27d23
Johan Degraeve <johan.degraeve@gmail.com>
2024-04-02 23:43:26 +0200

    version 1.2.7

--- src/main/BackupMainClass.java
+++ src/main/BackupMainClass.java
@@ -13,7 +13,7 @@ public class BackupMainClass {
 	
     public static void main(String[] args) {
     	
-    	System.out.println("version 1.2.6");
+    	System.out.println("version 1.2.7");
         
     	CommandLineArguments commandLineArguments = CommandLineArguments.getInstance(args);
 
